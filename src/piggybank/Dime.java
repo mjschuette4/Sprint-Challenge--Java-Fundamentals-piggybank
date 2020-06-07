05a380f0f1e0049832eb9a53a2811e4c40cafcaf
MatthewSchuette <mjschuette4@gmail.com>
2020-06-07 17:58:51 -0500

    Completed other coins

--- src/piggybank/Dime.java
+++ src/piggybank/Dime.java
@@ -1,5 +1,46 @@
 package piggybank;
 
 public class Dime extends AbstractMoney {
-    
+    private double coinValue = 0.10;
+    private String coinName = "Dime";
+    private int coinQuantity;
+    private double coinBalance;
+
+    public Dime(int coinQuantity) {
+        this.coinQuantity = coinQuantity;
+    }
+
+    public Dime() {
+        this.coinQuantity = 1;
+    }
+
+    public void setName(String coinName) {
+        this.coinName = coinName;
+    }
+
+    public void setQuantity(int coinQuantity) {
+        this.coinQuantity = coinQuantity;
+    }
+
+    public void setBalance( int coinQuantity) {
+        this.coinBalance = this.coinQuantity * this.coinValue;
+    }
+
+    @Override
+    public String getCoinName() {
+        return coinName;
+    }
+
+    public int getcoinQuantity() {
+        return coinQuantity;
+    }
+
+    public double getCoinBalance() {
+        return coinQuantity * coinValue;
+    }
+
+    @Override
+    public String toString() {
+        return "$" + coinQuantity;
+    }
 }
--- src/piggybank/Quarter.java
+++ src/piggybank/Quarter.java
@@ -1,5 +1,46 @@
 package piggybank;
 
 public class Quarter extends AbstractMoney {
-    
+    private double coinValue = 0.25;
+    private String coinName = "Quarter";
+    private int coinQuantity;
+    private double coinBalance;
+
+    public Quarter(int coinQuantity) {
+        this.coinQuantity = coinQuantity;
+    }
+
+    public Quarter() {
+        this.coinQuantity = 1;
+    }
+
+    public void setName(String coinName) {
+        this.coinName = coinName;
+    }
+
+    public void setQuantity(int coinQuantity) {
+        this.coinQuantity = coinQuantity;
+    }
+
+    public void setBalance( int coinQuantity) {
+        this.coinBalance = this.coinQuantity * this.coinValue;
+    }
+
+    @Override
+    public String getCoinName() {
+        return coinName;
+    }
+
+    public int getcoinQuantity() {
+        return coinQuantity;
+    }
+
+    public double getCoinBalance() {
+        return coinQuantity * coinValue;
+    }
+
+    @Override
+    public String toString() {
+        return "$" + coinQuantity;
+    }
 }
