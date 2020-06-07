df0fb6c00863e2d1c501ba5ce95da440ab0a344c
MatthewSchuette <mjschuette4@gmail.com>
2020-06-07 17:45:11 -0500

    AbstractMoney built out

--- src/piggybank/AbstractMoney.java
+++ src/piggybank/AbstractMoney.java
@@ -2,4 +2,32 @@ package piggybank;
 
 public abstract class AbstractMoney {
     
+    int coinQuantity;
+    double coinValue;
+    String coinName;
+    double coinBalance;
+
+    public AbstractMoney(int coinQuantity) {
+        this.coinQuantity = coinQuantity;
+    }
+
+    public AbstractMoney() {
+        coinQuantity = 1;
+    }
+
+    public int getCoinQuantity() {
+        return coinQuantity;
+    }
+
+    public double getCoinValue() {
+        return coinValue;
+    }
+
+    public String getCoinName() {
+        return coinName;
+    }
+
+    public double getCoinBalance() {
+        return coinValue * coinQuantity;
+    }
 }
---
+++ src/piggybank/Main.java
@@ -0,0 +1,7 @@
+package piggybank;
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
